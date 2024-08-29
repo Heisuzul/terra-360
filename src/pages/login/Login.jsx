325f9834600f606e9699210fab5c6a1617e5b5b4
Heisuzul <heidy.mina@correounivalle.edu.co>
2024-08-28 23:28:55 -0500

    Login

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useCallback } from 'react';
+import { useAuthStore } from '../../stores/use-auth-store';
+
+function Login() {
+    const { user, observeAuthState, loginGoogleWithPopup, logout } = useAuthStore();
+
+    useEffect(() => {
+        observeAuthState();
+    }, [observeAuthState]);
+
+    const handleLogin = useCallback(() => {
+        loginGoogleWithPopup();
+    }, [loginGoogleWithPopup]);
+ 
+    const handleLogout = useCallback(() => {
+        logout();
+    }, [logout]);
+
+    return (
+        <div className="container-login">
+            {user ? (
+                <>
+                    <div className='world-container'>
+                        <p className="welcome-text">Welcome, {user.displayName}</p>
+                        <button className="logout-button" onClick={handleLogout}>
+                            Logout
+                        </button>
+                    </div>
+                </>
+            ) : (
+                <button className="login-button" onClick={handleLogin}>
+                    Login with Google
+                </button>
+            )}
+        </div>
+    );
+}
+
+export default Login;
