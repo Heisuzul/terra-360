09b7d126731f636059e2c103b8a3dc8bc66358b6
Heisuzul <heidy.mina@correounivalle.edu.co>
2024-09-02 22:28:11 -0500

    Routes

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,29 +1,33 @@
 import { useEffect, useCallback } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../../stores/use-auth-store';
-import World from '../../r3f/scenes/World';
+import World from '../../r3f/scenes/World.jsx';
 import './Login.css'
 
 function Login() {
     const { user, observeAuthState, loginGoogleWithPopup, logout } = useAuthStore();
+    const navigate = useNavigate();
 
     useEffect(() => {
         observeAuthState();
     }, [observeAuthState]);
 
-    const handleLogin = useCallback(() => {
-        loginGoogleWithPopup();
-    }, [loginGoogleWithPopup]);
+    const handleLogin = useCallback(async () => {
+          await loginGoogleWithPopup(); // Espera a que se complete la autenticación
+          navigate('/world'); // Navega a "/about" después de la autenticación
+      }, [loginGoogleWithPopup, navigate]);
  
-    const handleLogout = useCallback(() => {
-        logout();
-    }, [logout]);
+    const handleLogout = useCallback(async() => {
+        await logout();
+        navigate('/'); // Navega a "/" después de desloguearse
+    }, [logout], [navigate]);
 
     return (
         <div className="container-login">
             {user ? (
                 <>
                     <div className='world-container'>
-                        <World />
+                        <World/>
                         <div className="welcome-div">
                         <p className="welcome-text">Welcome, {user.displayName}</p>
                         <button className="logout-button" onClick={handleLogout}>
