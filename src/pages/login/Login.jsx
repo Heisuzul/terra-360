29fb6b229065aeffa0d92017963aed0cbb6694bf
manuel <medina.manuel@correounivalle.edu.co>
2024-09-03 03:00:43 +0000

    Added database logic to manage users who authenticate with google

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,53 @@
+import './Login.css'
+import { useEffect, useCallback } from 'react';
+import { useAuthStore } from '../../store/use-auth-store';
+import UserDAO from '../../DAO/UserDAO';
+
+function Login() {
+    const { user, observeAuthState, loginGoogleWithPopup, logout } = useAuthStore();
+
+    useEffect(() => {
+        observeAuthState();
+    }, [observeAuthState]);
+
+    useEffect(() => {
+        if (user){
+            const newUser ={
+                email: user.email,
+                name: user.name,
+                photo:user.photoURL
+            };
+            UserDAO.createUser(newUser)
+        }
+    }, [user]);
+
+    const handleLogin = useCallback(() => {
+        loginGoogleWithPopup();
+    }, [loginGoogleWithPopup]);
+ 
+    const handleLogout = useCallback(() => {
+        logout();
+    }, [logout]);
+
+    return (
+        <div className="container-login">
+            {user ? (
+                <>
+                    <div className='world-container'>
+                        <p className="welcome-text">Welcome, {user.displayName}</p>
+                        <button className="logout-button" onClick={handleLogout}>
+                            Logout
+                        </button>
+                        {/*<World />*/}
+                    </div>
+                </>
+            ) : (
+                <button className="login-button" onClick={handleLogin}>
+                    Login with Google
+                </button>
+            )}
+        </div>
+    );
+}
+
+export default Login;
