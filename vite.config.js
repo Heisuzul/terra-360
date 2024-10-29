54ddc996c15ee826947c35a5e9630bf4d3dc1127
Heisuzul <heidy.mina@correounivalle.edu.co>
2024-10-28 19:26:22 -0500

    fixes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Esto permite que el servidor escuche en todas las interfaces
+    port: 9000         // Puedes cambiar el puerto si lo necesitas
+  }
 });
