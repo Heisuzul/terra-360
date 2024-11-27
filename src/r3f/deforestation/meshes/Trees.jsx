df80f86c915e874a9fb1f2c778ff23046e401ad0
Julian Puyo <112900679+puyojulian@users.noreply.github.com>
2024-11-26 23:57:16 -0500

    improved the saturation value selection, it will not go now lower than -1

--- src/r3f/deforestation/meshes/Trees.jsx
+++ src/r3f/deforestation/meshes/Trees.jsx
@@ -193,7 +193,7 @@ const Trees = forwardRef(({
     <>
       <EffectComposer>
         <Vignette offset={0.1} darkness={intensity*1.1} />
-        <HueSaturation hue={0.01} saturation={-0.1 - intensity/1.2} />
+        <HueSaturation hue={0.01} saturation={Math.max(-0.1 - intensity/1.2, -1)} />
         {/* <BrightnessContrast brightness={0} contrast={0.2} /> */}
         <Bloom intensity={0.1}/>
       </EffectComposer>
